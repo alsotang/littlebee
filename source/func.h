d7eb01720ac09d5385a64f1461c3f9f4c2321809
alsotang <alsotang@gmail.com>
2012-03-25 16:48:35 +0800

    func.h的定义和main.c的简单设计。

---
+++ source/func.h
@@ -0,0 +1,36 @@
+//画图相关的函数：
+//初始化的时候画边框
+void drawFrame(int hight, int width);
+
+
+//初始化的时候画所有的蜜蜂
+void drawAllBees(int** bee_position, int hight, int width);
+
+//根据枪的坐标画出枪的位置
+void drawGun(int gun_x, int gun_y);
+
+void cleanGun(int gun_x, int gun_y);
+//根据单个小蜜蜂的位置把它在屏幕上删除掉
+void killBee(int bee_x, int bee_y);
+
+
+
+//按键有关的函数：
+//检测到' '就传给spacePressed, 检测到`上下左右`就传给directionPressed
+//这个函数要循环执行
+void listenKeyboard(void);
+
+//gun_x AND gun_y用来确定枪从哪里发出子弹
+void spacePressed(void, int gun_x, int gun_y);
+
+//gun_x AND gun_y用来确定从哪里开始消除并移动枪
+void directionPressed(void, int gun_x, int gun_y);
+
+
+
+
+
+
+
+
+
---
+++ source/main.c
@@ -0,0 +1,15 @@
+int g_gun_x = 0, g_gun_y = 0;
+int g_hight = 10, g_width = 7;
+
+//蜜蜂群的宽度和高度
+int bee_position[g_width - 2][5];
+
+
+void init(void) {
+}
+
+int main(int argc, const char *argv[])
+{
+    
+    return 0;
+}
